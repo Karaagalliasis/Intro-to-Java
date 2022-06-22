d829f6555ccb7f3a9227cdf1de713fde0bb2a6ef
Agata Uzoamaka Arukweh <105239530+Karaagalliasis@users.noreply.github.com>
2022-06-22 11:09:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+// this is my first javascript code!
+document.getElementById("demo1"). innerHTML = "My name is Chara-Agalliasis Uzoamaka Arukweh.";
+document.getElementById("demo2"). innerHTML = "I'm" + " " + 5.6 + "ft" + " " + "tall.";
+document.getElementById("demo3"). innerHTML = "I'm from Nigeria.";
