8d931f9d9faf2ef3251450c8cf562e0ae5d0a7e3
Agata Uzoamaka Arukweh <105239530+Karaagalliasis@users.noreply.github.com>
2022-06-23 09:33:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // this is my first javascript code!
+
+var myName = "Chara-Agalliasis Uzoamaka Arukweh", myHeight = 5.6, myCountry = "Nigeria";
+
+alert(myName + "," + "" + myHeight + "," + "" + myCountry);
+
 document.getElementById("demo1"). innerHTML = "My name is Chara-Agalliasis Uzoamaka Arukweh.";
 document.getElementById("demo2"). innerHTML = "I'm" + " " + 5.6 + "ft" + " " + "tall.";
 document.getElementById("demo3"). innerHTML = "I'm from Nigeria.";
